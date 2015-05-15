d0d007e32753c1ad133d7af5e5c2d06075b20553
Kerry Scharfglass <kerry@mindtribe.com>
2015-05-15 09:19:11 -0700

    small tweaks trying to find bugs

--- keyboard/trainboard/config.h
+++ keyboard/trainboard/config.h
@@ -35,12 +35,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 /* Set 0 if debouncing isn't needed */
+//FIXME do I need this?
 #define DEBOUNCE    5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
 
 /* key combination for command */
 #define IS_COMMAND() ( \
