50567b967b1d5acb84b5c151906bf044c455d990
Kerry Scharfglass <iborgel@gmail.com>
2015-05-21 23:31:55 -0700

    added mousekeys, other small keymap changes

--- keyboard/trainboard/keymap_brownfox-mod.c
+++ keyboard/trainboard/keymap_brownfox-mod.c
@@ -6,19 +6,20 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,      \
           LCTL, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,            \
           LSFT, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,            \
-          LCTL, LGUI, LALT, SPC,             SPC,  RGUI, RALT, FN1, RCTL),
+          FN2, LGUI, LALT, SPC,             SPC,  RGUI, RALT, FN1, RCTL),
 
    /* 1: FN 1 */
    KEYMAP(MUTE, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  VOLU, VOLD, \
-          TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PGUP, UP,   TRNS, DEL,  \
-          TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, TRNS, LEFT, RGHT, TRNS,  \
-          TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, END , PGDN,  DOWN, TRNS, TRNS, \
-          TRNS, TRNS, TRNS, TRNS,             TRNS, TRNS, TRNS, TRNS, TRNS),
+          TRNS, WH_U, MS_U, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, PGUP, UP,   TRNS, DEL,  \
+          TRNS, MS_L, MS_D, MS_R, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, RGHT, TRNS,  \
+          TRNS, WH_D, BTN2, BTN3, TRNS, TRNS, TRNS, TRNS, END , PGDN,  DOWN, TRNS, TRNS, \
+          TRNS, TRNS, TRNS, BTN1,             TRNS, TRNS, TRNS, TRNS, TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
    [0] = ACTION_LAYER_MOMENTARY(1),    // switch to FN layer on press and hold
    [1] = ACTION_LAYER_TOGGLE(1),       // toggle to FN layer until tapped again
+   [2] = ACTION_LAYER_MOMENTARY(1),    // switch to FN layer on press and hold
 
    // TODO add tap actions for missing keys?
 
