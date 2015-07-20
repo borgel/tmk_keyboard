18090aa84304e121977cb0688b0aadd962546afb
Kerry Scharfglass <iborgel@gmail.com>
2015-07-19 22:30:50 -0700

    Working! Had 2 shorts

--- keyboard/trainboard1.5/config.h
+++ keyboard/trainboard1.5/config.h
@@ -43,7 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 //#define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
+/* magic key combination for command */
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
