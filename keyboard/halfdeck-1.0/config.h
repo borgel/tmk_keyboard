1b9d300aa56c3f0bf0e13f498a58947945a8a4a7
Kerry Scharfglass <iborgel@gmail.com>
2015-11-07 15:08:08 -0800

    Move bootloader trigger key to 'v' from 'b'

--- keyboard/halfdeck-1.0/config.h
+++ keyboard/halfdeck-1.0/config.h
@@ -38,6 +38,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_KEY_SALT              KC_ESC        //we can't hold space on the halfdeck
 #define BOOTMAGIC_KEY_SKIP              KC_SPACE
 
+// used to trigger bootloader (to reprogram)
+#define BOOTMAGIC_KEY_BOOTLOADER        KC_V
+
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
 
