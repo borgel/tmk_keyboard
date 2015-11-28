5b04dfe143335e8d2c2bd26bcb5e448a22d216e3
Kerry Scharfglass <iborgel@gmail.com>
2015-11-27 22:23:34 -0800

    Cleaning out cruft

--- keyboard/fullmonty-1.0/config.h
+++ keyboard/fullmonty-1.0/config.h
@@ -21,27 +21,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6057
+#define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    t.m.k.
-#define PRODUCT         Phantom
+#define MANUFACTURER    btg-limited
+#define PRODUCT         Fullmonty-V1.0
+#define DESCRIPTION     A bright orange scratch built ANSI 104 keyboard
 
 /* message strings */
-#define DESCRIPTION     t.m.k. keyboard firmware for Phantom
+#define DESCRIPTION     t.m.k. keyboard firmware for Fullmonty
 
 /* matrix size */
 #define MATRIX_ROWS 6
-#define MATRIX_COLS 17
+#define MATRIX_COLS 18
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
 /* Set 0 if need no debouncing */
-#define DEBOUNCE    7
+#define DEBOUNCE    5
 
 /* Set LED brightness 0-255.
  * This have no effect if sleep LED is enabled. */
-#define LED_BRIGHTNESS  250
+//#define LED_BRIGHTNESS  250
 
 /* key combination for command */
 #define IS_COMMAND() ( \
